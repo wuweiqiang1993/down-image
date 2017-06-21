77116b1432ece3cfc105fee097556684cd3a72ca
wuweiqiang1993 <875801277@qq.com>
2017-06-21 23:27:04 +0800

    Update image_file_down.php

--- image_file_down.php
+++ image_file_down.php
@@ -2,7 +2,7 @@
     set_time_limit(0);
     ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)'); 
     /*
-    ** 获取网址图片
+    ** 获取网址内容
     **
     */
     function getContent($url, $method = 'GET', $postData = array()) {
