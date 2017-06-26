937bbe301003e8ed1f1d69c13a2e7e85fcc73ebc
wuweiqiang1993 <875801277@qq.com>
2017-06-26 21:50:50 +0800

    改为CLI模式运行，加快运行速度

--- image_file_down.php
+++ image_file_down.php
@@ -1,10 +1,23 @@
+#!G:\phpStudy\php\php-7.0.12-nts\php.exe -q
 <?php
-    set_time_limit(0);
-    ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)'); 
-    /*
-    ** 获取网址内容
-    **
-    */
+    function deleteAll($path) {
+        $op = dir($path);
+        while(false != ($item = $op->read())) {
+            if($item == '.' || $item == '..') {
+                continue;
+            }
+            if(is_dir($op->path.'/'.$item)) {
+                deleteAll($op->path.'/'.$item);
+                rmdir($op->path.'/'.$item);
+            } else {
+                unlink($op->path.'/'.$item);
+            }
+        
+        }   
+    }
+    if(file_exists('./new_mzitu1/')){
+        deleteAll('./new_mzitu1/');
+    } 
     function getContent($url, $method = 'GET', $postData = array()) {
         $curl = curl_init();
         curl_setopt ($curl, CURLOPT_URL, $url);
@@ -17,12 +30,13 @@
         curl_close($curl);
         return $content;
     }
+    $STDOUT = fopen('php://stdout', 'w');
     for ($pageno = 1 ; $pageno < 147; $pageno ++) {
         if(!file_exists('./new_mzitu'.$pageno.'/')){
-            mkdir('./new_mzitu'.$pageno.'/');//按网站分页分存放目录
+            mkdir('./new_mzitu'.$pageno.'/');
         }
         $check = getContent('http://www.mzitu.com/page/'.$pageno);
-        preg_match_all('/<li><a\shref=\"(.*?)\"\starget=\"_blank\"><img/',$check,$matches);//获得内容页链接
+        preg_match_all('/<li><a\shref=\"(.*?)\"\starget=\"_blank\"><img/',$check,$matches);
         foreach ($matches[1] as $url) {
             //echo $url;
             for ($i=1; $i < 100; $i++) { 
@@ -30,7 +44,8 @@
                 if($content===FALSE){
                     break;
                 }
-                preg_match_all('/<img\ssrc=\"(.*?)\"/',$content,$matche);//获得图片链接
+                preg_match_all('/<img\ssrc=\"(.*?)\"/',$content,$matche);
+                //preg_match_all('/class=\"joke-main-img\" src=\"(.*?)\"/',$content,$matches);
                 foreach ($matche[1] as $img_url) {
                     //echo $url.'<br>';
                     $img = getContent($img_url);
@@ -38,8 +53,10 @@
                         continue;
                     }
                     file_put_contents('./new_mzitu'.$pageno.'/'.basename($img_url),$img);
-                    //sleep(1);
+                    fwrite($STDOUT,basename($img_url)." Done\n");
                 }
             }
         }
     }
+    fwrite($STDOUT,"All Done\n");
+    fclose($STDOUT);
